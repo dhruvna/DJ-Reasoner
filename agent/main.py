197ccf554e7c56c1efc30c75d4e7013b094a7cab
Dhruv Aggarwal <dna@ucsb.edu>
2025-05-29 10:48:22 -0700

    Sanity check for main.py

--- agent/main.py
+++ agent/main.py
@@ -3,3 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 print("Spotify Client ID:", os.getenv("SPOTIFY_CLIENT_ID"))
+print("Spotify Client Secret:", os.getenv("SPOTIFY_CLIENT_SECRET"))
+print("Spotify Redirect URI:", os.getenv("SPOTIFY_REDIRECT_URI"))
+print("OpenAI API Key:", os.getenv("OPENAI_API_KEY"))
